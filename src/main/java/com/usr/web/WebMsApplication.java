82d16d7c88f91578d2a52e5d9d8d6ad7234cd015
Utkarsh $ingh <utkarshsingh122096@gmail.com>
2023-05-03 00:16:05 +0530

    webapp added
    webapp added

---
+++ src/main/java/com/usr/web/WebMsApplication.java
@@ -0,0 +1,13 @@
+package com.usr.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMsApplication.class, args);
+	}
+
+}
